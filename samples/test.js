684a7167df40a694c6bb8253d702ecf7c67ea48d
Dmitri Gaskin <dmitrig01@gmail.com>
2015-04-29 01:44:24 -0700

    a little more jsx

--- samples/test.js
+++ samples/test.js
@@ -2,22 +2,20 @@
 import { Namespace, Storage, SprintComponent, wrap, k, a } from '../index';
 import React from 'react';
 
-class TestComponent extends SprintComponent {
-	render() {
-		return <div>Hello {this.props.blackberry_url}</div>;
-	}
-}
-
 var storage = new Storage();
 var appNS = new Namespace();
 appNS.set([ 'a' ], { 'blackberry_url': 'ABC' });
 appNS.set([ 'b' ], { 'blackberry_url': 'DEF' });
 storage.register('app', appNS);
 
+class TestComponent extends SprintComponent {
+	render() {
+		return <div>Hello {this.props.blackberry_url}</div>;
+	}
+}
 var T = wrap(TestComponent, {
 	props: {
 		"blackberry_url": k("app", k("props", "app_id"), "blackberry_url")
-		// "default_url": k("app", app_id_key, "default_url"),
 	},
 	actions: {
 		"save": a("app", "save")
@@ -25,7 +23,6 @@ var T = wrap(TestComponent, {
 });
 
 window.onload = function() {
-	var e = React.createElement(T, { "storage": storage, "app_id": "b" });
-	React.render(e, document.getElementById('test'));
+	React.render(<T storage={storage} app_id="b" />, document.getElementById('test'));
 }
 
