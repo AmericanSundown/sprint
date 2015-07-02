777e1eaf40773718698c77c214ca972175f22888
Dmitri Gaskin <dmitrig01@gmail.com>
2015-07-02 01:27:27 -0700

    export PollingServerNamespace

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Storage from './Storage';
 import Namespace from './Namespace';
 import ServerNamespace from './ServerNamespace';
 import ReadOnlyServerNamespace from './ReadOnlyServerNamespace';
+import PollingServerNamespace from './PollingServerNamespace';
 import Server from './Server';
 import key from './key';
 import action from './action';
@@ -9,4 +10,4 @@ import { SprintComponent, wrap } from './component';
 
 import EventSystem from './EventSystem';
 
-export { key as k, action as a, Server, Storage, Namespace, ServerNamespace, ReadOnlyServerNamespace, SprintComponent, wrap, EventSystem };
+export { key as k, action as a, Server, Storage, Namespace, ServerNamespace, ReadOnlyServerNamespace, PollingServerNamespace, SprintComponent, wrap, EventSystem };
